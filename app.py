d74c44f664d3cb0feead5dd51b25a1639e60789f
mattg317 <mattg317@gmail.com>
2018-07-31 10:08:03 -0400

    updates to bot

--- app.py
+++ app.py
@@ -1,20 +1,17 @@
 import os
 import pandas as pd
-import spacy
 import tweepy
 import time
+import spacy
 import en_core_web_sm
 import matplotlib
-matplotlib.use('Agg')
+
+matplotlib.use("Agg")
 
 
-# For testing locally
-# from config import (consumer_key,
-#                     consumer_secret,
-#                     access_token,
-#                     access_token_secret)
+# Uncomment the following to test the script locally:
+# from config import consumer_key, consumer_secret, access_token, access_token_secret
 
-# For deploying to Heroku
 # Get config variable from environment variables
 consumer_key = os.environ.get("consumer_key")
 consumer_secret = os.environ.get("consumer_secret")
@@ -32,43 +29,64 @@ nlp = en_core_web_sm.load()
 
 def update_twitter():
 
-    # Create dictionary to hold text and label entities# Creat
-    tweet_dict = {
-        "text": [],
-        "label": [],
-    }
+    # Create dictionary to hold text and label entities
+    tweet_dict = {"text": [], "label": []}
 
-    # Get tweets from a users timeline
-    user_tweets = api.user_timeline("EpcotCentre")
+    mentions = api.search(q="@TraderData Analyze:")
+    print(mentions)
+    words = []
+    try:
+        command = mentions["statuses"][0]["text"]
+        words = command.split("Analyze:")
+        target_account = words[1].strip()
+        user_tweets = api.user_timeline(target_account, page=1)
 
-    # Loop through tweets
-    for tweet in user_tweets:
+        # Loop through tweets
+        for tweet in user_tweets:
 
-        # Use nlp on each tweet
-        doc = nlp(tweet["text"])
+            # Use nlp on each tweet
+            doc = nlp(tweet["text"])
 
-        # Check if nlp returns no entities
-        if not doc.ents:
-            print("No entities to visualize")
-            print("----------------------------")
+            # Check if nlp returns no entities
+            if not doc.ents:
+                print("No entities to visualize")
+                print("----------------------------")
+            else:
+                # Print the entities for each doc
+                for ent in doc.ents:
+                    # Store entities in dictionary
+                    tweet_dict["text"].append(ent.text)
+                    tweet_dict["label"].append(ent.label_)
+        # Convert dictionary to DataFrame
+        tweet_df = pd.DataFrame(tweet_dict)
+        tweet_df.head()
+
+        # Group by labels# Group
+        label_frequency = tweet_df.groupby(["label"]).count()
+
+        # Get bar graph as a figure and tweet chart
+        bar = label_frequency.plot.bar()
+        fig = bar.get_figure()
+        fig.savefig("box.png")
+        api.update_with_media(
+            "box.png", "Break down of tweet labels"
+        )
+    except Exception:
+        raise
+
+    # Grab Self Tweets
+    tweets = api.user_timeline()
+
+    # Confirm the target account has never been tweeted before
+    repeat = False
+
+    for tweet in tweets:
+        if target_account in tweet["text"]:
+            repeat = True
+            print("Sorry. Repeat detected!")
+
         else:
-            # Print the entities for each doc
-            for ent in doc.ents:
-                # Store entities in dictionary
-                tweet_dict["text"].append(ent.text)
-                tweet_dict["label"].append(ent.label_)
-    # Convert dictionary to DataFrame
-    tweet_df = pd.DataFrame(tweet_dict)
-    tweet_df.head()
-
-    # Group by labels# Group
-    label_frequency = tweet_df.groupby(['label']).count()
-
-    # Get bar graph as a figure and tweet chart
-    bar = label_frequency.plot.bar()
-    fig = bar.get_figure()
-    fig.savefig('box.png')
-    api.update_with_media("box.png", "Break down of tweet labels")
+            continue
 
 
 # Have the Twitter bot update once a day for a week
@@ -80,7 +98,7 @@ while days < 7:
     update_twitter()
 
     # Wait a day
-    time.sleep(86400)
+    time.sleep(300)
 
     # Update day counter
     days += 1
